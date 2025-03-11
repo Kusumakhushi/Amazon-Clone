693337313ab72e60277e15e703f818eef525e814
Kusumakhushi <kusumac157@gmail.com>
2025-03-11 14:39:46 +0530

    commit message

---
+++ practicefolder/prac.js
@@ -0,0 +1,22 @@
+//  prcti 2
+// let gn=25;
+// let us=prompt("guess the no");
+// while(us!=gn){
+//     us=prompt("you entered wrong no,guess again");
+// }
+// console.log("congrulations u entered right no");
+
+// prac 1
+
+// for(i=1;i<=100;i++){
+//     if(i%2 ==0){
+// console.log("i=",i)
+//     }3
+//}
+
+// let str="kusuma lioness"
+// console.log(str.length);
+
+let str="anpahjdehnfr";
+console.log(str.toLowerCase())
+console.log(str.toUpperCase())
